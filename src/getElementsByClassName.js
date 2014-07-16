ee099c8e3daedb203e9ef42bc3bafb37f7240b9b
Gabs00 <gewen87@gmail.com>
2014-07-16 13:31:48 -0400

    implimented getElementsByClassName()

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
+var getElementsByClassName = function(classes, elem){
+    if(!elem){
+        elem = document.body;   
+    }
+    var results = [];
+    var target_classes = classes.split(' ');
+    var class_list = elem.classList;
+    if(class_list){
+        if(check_classes(target_classes, class_list)){
+            results.push(elem);   
+        }
+    }
+    if(elem.hasChildNodes()){
+        var children = elem.childNodes;
+        for(var i = 0; i < children.length; i++){
+              var matches = getElementsByClassName(classes, children[i]);
+              transfer(results, matches);
+        }
+        
+    }
+    return results;
+
+	function transfer(in_array, out_array){
+	    var item;
+	    while(item = out_array.shift()){
+	        in_array.push(item);   
+	    }
+	}
+	function check_classes(classes, class_list){
+	    for(var i = 0; i < classes.length; i++){
+	        if(!class_list.contains(classes[i])){
+	            return false;   
+	        }
+	    }
+	    return true;
+	}
 };
